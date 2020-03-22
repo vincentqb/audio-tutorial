27f8d738dba9a9ffd32a41b16ce5b26142683a72
Vincent Quenneville-Belair <vincentqb@gmail.com>
2020-03-22 06:44:34 -0700

    parse arg.

--- PipelineTrain.py
+++ PipelineTrain.py
@@ -48,7 +48,7 @@ parser = argparse.ArgumentParser(add_help=False, allow_abbrev=False)
 # In[ ]:
 
 
-parser.add_argument("--batch_size", default=None, type=int, default=64)
+parser.add_argument("--batch_size", type=int, default=64)
 
 args, _ = parser.parse_known_args()
 
