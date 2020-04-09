82491b2a4f5fe7b72ec32bea45c7041b9476795b
Vincent Quenneville-Belair <vincentqb@gmail.com>
2020-04-09 15:34:57 -0700

    remove duplicate env var.

--- batch.sh
+++ batch.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-# The ENV below are only used in distributed training with env:// initialization
-export MASTER_ADDR=${SLURM_NODELIST:0:9}${SLURM_NODELIST:10:4}
-export MASTER_PORT=29500
-
 for arch in 'wav2letter' 'lstm'; do
     for bs in 1 256 2048; do
         for lr in 1. .01 .0001; do
