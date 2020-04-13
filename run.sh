6ef03d04d39965a07ebc6efc89de7a80f9f5bbbc
Vincent Quenneville-Belair <vincentqb@gmail.com>
2020-04-13 14:23:53 -0700

    Change order sbatch.

--- run.sh
+++ run.sh
@@ -5,11 +5,14 @@
 #SBATCH --error=/checkpoint/%u/jobs/audio-%j.err
 #SBATCH --signal=USR1@600
 #SBATCH --open-mode=append
-#SBATCH --ntasks-per-node=1
 #SBATCH --partition=learnfair
-#SBATCH --cpus-per-task=80
 #SBATCH --time=30:00:00
 #SBATCH --mem-per-cpu=5120
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:8
+#SBATCH --cpus-per-task=80
+# 2x (number of data workers + number of GPUs requested)
 
 arch=$1
 bs=$2
