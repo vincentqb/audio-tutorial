f56b805e253c0732db00eb5e18428d471fea04d5
Vincent Quenneville-Belair <vincentqb@gmail.com>
2020-04-13 14:24:32 -0700

    use newer slurm name.

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ bs=$2
 lr=$3
 
 # The ENV below are only used in distributed training with env:// initialization
-export MASTER_ADDR=${SLURM_NODELIST:0:9}${SLURM_NODELIST:10:4}
+export MASTER_ADDR=${SLURM_JOB_NODELIST:0:9}${SLURM_JOB_NODELIST:10:4}
 export MASTER_PORT=29500
 
 if [ "$bs" -le "8" ]
@@ -31,8 +31,8 @@ else
     gpus=8
 fi
 
-srun --label --nodes=$nodes --gres=gpu:$gpus \
+srun --label \
     python /private/home/vincentqb/experiment/PipelineTrain.py \
 	--arch $arch --batch-size $bs --learning-rate $lr \
-	--resume /private/home/vincentqb/experiment/checkpoint-$arch-$bs-$lr.pth.tar
-	# --world-size $SLURM_NNODES --dist-url 'env://' --dist-backend='nccl'
+	--resume /private/home/vincentqb/experiment/checkpoint-$SLURM_JOB_ID-$arch-$bs-$lr.pth.tar \
+	--world-size $SLURM_JOB_NUM_NODES --dist-url 'env://' --dist-backend='nccl'
