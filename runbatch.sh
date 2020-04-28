3d1a85669af754f80566251812aaca0be0cdc94d
Vincent Quenneville-Belair <vincentqb@gmail.com>
2020-04-28 10:34:28 -0700

    experiment with learning rate only, without distributed.

--- runbatch.sh
+++ runbatch.sh
@@ -6,13 +6,13 @@
 #SBATCH --signal=USR1@600
 #SBATCH --open-mode=append
 #SBATCH --partition=learnfair
-#SBATCH --time=30:00:00
+#SBATCH --time=4320
 #SBATCH --mem-per-cpu=5120
-#SBATCH --nodes=8
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:8
 #SBATCH --cpus-per-task=80
-#SBATCH --array=1-18
+#SBATCH --array=1-3
 # number of CPUs = 2x (number of data workers + number of GPUs requested)
 
 if [[ "$SLURM_ARRAY_TASK_COUNT" -ne $((2 * 3 * 3)) ]]; then
@@ -21,9 +21,9 @@ if [[ "$SLURM_ARRAY_TASK_COUNT" -ne $((2 * 3 * 3)) ]]; then
 fi
 
 i=1
-for arch in 'wav2letter' 'lstm'; do
-    for bs in 128 256 512; do
-        for lr in 1. .01 .0001; do
+for arch in 'wav2letter'; do
+    for bs in 128; do
+        for lr in 1. .1 .01; do
             if [[ "$i" == "$SLURM_ARRAY_TASK_ID" ]]; then break; fi
             ((i++))
         done
